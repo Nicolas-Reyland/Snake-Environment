1d9907b657df61013bd4033380437749af40f218
Ilu-Vatar <43726409+Ilu-Vatar@users.noreply.github.com>
2019-04-08 12:48:00 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+import setuptools
+
+setup(name='snake',
+      version='1.0',
+      description='A Snake environment',
+      author='Nicolas Reyland',
+      author_email='nicolas@reyland.net',
+      url='https://github.com/Ilu-Vatar/Snake-Environment',
+      packages=setuptools.find_packages()
+     )
